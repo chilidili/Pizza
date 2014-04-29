aaec8bafd74c8951b5032d70d1a7eb7ee97ff12c
Dylan Summers <dylansummers@Dylans-MacBook-Pro.local>
2014-04-29 11:59:14 -0400

    Initial Commit

---
+++ Pizza/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Pizza
+//
+//  Created by Dylan Summers on 4/29/14.
+//  Copyright (c) 2014 Dylan Summers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
